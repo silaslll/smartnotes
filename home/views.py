a2c1f0b9c61772a07a3563078e7ff0449c86af6a
silaslll <lihangyi01@gmail.com>
2022-09-16 14:36:59 -0400

    add home page

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+#use the bracket to pass imformation for the html pages
+def home(request):
+    return render(request, 'home/welcome.html',{'today' : datetime.today()})

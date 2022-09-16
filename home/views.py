495a4eec9b2a21568949a3a70ef1fbd52e19e4b0
silaslll <lihangyi01@gmail.com>
2022-09-16 15:18:32 -0400

    modularization

--- home/views.py
+++ home/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from datetime import datetime
 
 # Create your views here.
-#use the bracket to pass imformation for the html pages
+# Use the bracket to pass imformation for the html pages
 def home(request):
     return render(request, 'home/welcome.html',{'today' : datetime.today()})
--- smartnotes/urls.py
+++ smartnotes/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from home import views
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home', views.home)
+    #if we delete the app, this won't give any error
+    path('', include('home.urls'))
 ]

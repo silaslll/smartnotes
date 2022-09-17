ba3baaaa7b1f1f9dc85a72880ba0f848a9cec304
silaslll <lihangyi01@gmail.com>
2022-09-17 14:03:41 -0400

    add detail page

--- notes/views.py
+++ notes/views.py
@@ -7,3 +7,7 @@ from .models import Notes
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html', {'notes' : all_notes})
+
+def detail(request, pk):
+    note  = Notes.objects.get(pk=pk)
+    return render(request, 'notes/notes_detail.html', {'note': note})

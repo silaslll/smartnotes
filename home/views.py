c35bcf5e906bdcc9fc3409ee3fcbf6c58c7ec7fa
silaslll <lihangyi01@gmail.com>
2022-09-17 12:33:38 -0400

    add authentication

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 # Use the bracket to pass imformation for the html pages
 def home(request):
     return render(request, 'home/welcome.html',{'today' : datetime.today()})
+
+@login_required
+def authorized(request):
+    return render(request, 'home/authorized.html', {})

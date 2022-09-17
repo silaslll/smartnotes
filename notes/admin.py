d2ab564dc9e9b3c04f0825cfc705f4ae78adf384
silaslll <lihangyi01@gmail.com>
2022-09-17 13:01:06 -0400

    Configure it to appear on the Django interface

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from . import models
+
 # Register your models here.
+class NotesAdmin(admin.ModelAdmin):
+    #we don't need any configuration on this admin model
+    pass
+
+admin.site.register(models.Notes, NotesAdmin)

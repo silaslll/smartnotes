080acff93cd3e8051ed55bad4e01265b822a2f62
silaslll <lihangyi01@gmail.com>
2022-09-17 17:10:25 -0400

    add to home page

--- notes/urls.py
+++ notes/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('notes', views.NotesListView.as_view()),
+    path('notes', views.NotesListView.as_view(), name = "notes.list"),
     path('notes/<int:pk>', views.NotesDetailView.as_view()),
 ]
